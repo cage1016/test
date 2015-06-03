3dc70c7e9f564d64e379ec960005b6e610255139
cage.chung <cage.chung@gmail.com>
2015-06-03 16:08:35 +0800

    add gcs csv.DictReader iterator. chunks read supported.

---
+++ tasks/GCSIterator.py
@@ -0,0 +1,104 @@
+import re
+import logging
+
+from apiclient.errors import HttpError
+
+
+DEFAULT_CHUNK_SIZE = 512 * 1024
+
+
+class GCSIterator(object):
+  """
+  Reference:
+  Parsing Large CSV Blobs on Google App Engine by Daniel Thompson @ d4nt
+  http://d4nt.com/parsing-large-csv-blobs-on-google-app-engine
+
+  Implement Google Cloud Storage csv.DictReader Iterator
+  """
+
+  def __init__(self, request, chunksize=DEFAULT_CHUNK_SIZE):
+    self._request = request
+    self._uri = request.uri
+    self._chunksize = chunksize
+    self._progress = 0
+    self._total_size = None
+    self._done = False
+
+    self._last_line = ""
+    self._line_num = 0
+    self._lines = []
+    self._buffer = None
+    self._done = False
+
+
+  def __iter__(self):
+    return self
+
+  def next(self):
+    if not self._buffer or len(self._lines) == (self._line_num + 1):
+      if self._lines:
+        self._last_line = self._lines[self._line_num]
+
+      if not self._done:
+        self._buffer, self._done = self.read()
+        logging.info('read chunks: %d%%' % int(float(self._progress) / float(self._total_size) * 100))
+
+      else:
+        self._buffer = ''
+
+      self._lines = re.split('\r|\n|\r\n', self._buffer)
+      self._line_num = 0
+
+      # Handle special case where our block just happens to end on a new line
+      if self._buffer[-1:] == "\n" or self._buffer[-1:] == "\r":
+        self._lines.append("")
+
+    if not self._buffer:
+      raise StopIteration
+
+    if self._line_num == 0 and len(self._last_line) > 0:
+      # print 'fixing'
+      result = self._last_line + self._lines[self._line_num] + "\n"
+
+    else:
+      result = self._lines[self._line_num] + "\n"
+
+    self._line_num += 1
+
+    return result
+
+
+  def read(self, num_retries=0):
+    headers = {
+      'range': 'bytes=%d-%d' % (
+        self._progress, self._progress + self._chunksize)
+    }
+    http = self._request.http
+
+    for retry_num in xrange(num_retries + 1):
+      if retry_num > 0:
+        self._sleep(self._rand() * 2 ** retry_num)
+        logging.warning(
+          'Retry #%d for media download: GET %s, following status: %d'
+          % (retry_num, self._uri, resp.status))
+
+      resp, content = http.request(self._uri, headers=headers)
+      if resp.status < 500:
+        break
+
+    if resp.status in [200, 206]:
+      if 'content-location' in resp and resp['content-location'] != self._uri:
+        self._uri = resp['content-location']
+      self._progress += len(content)
+
+      if 'content-range' in resp:
+        content_range = resp['content-range']
+        length = content_range.rsplit('/', 1)[1]
+        self._total_size = int(length)
+
+      if self._progress == self._total_size:
+        self._done = True
+      return content, self._done
+    else:
+      raise HttpError(resp, content, uri=self._uri)
+
---
+++ tasks/GCSIterator_test.py
@@ -0,0 +1,85 @@
+# coding: utf-8
+
+import sys
+import site
+import os
+import csv
+import httplib2
+
+
+PYTHON_VERSION = 'python%d.%d' % (sys.version_info[0], sys.version_info[1])
+
+
+def add(path, index=1):
+  """Insert site dir or virtualenv at a given index in sys.path.
+
+  Args:
+    path: relative path to a site dir or virtualenv.
+    index: sys.path position to insert the site dir.
+
+  Raises:
+    ValueError: path doesn't exist.
+  """
+  venv_path = os.path.join(path, 'lib', PYTHON_VERSION, 'site-packages')
+  if os.path.isdir(venv_path):
+    site_dir = venv_path
+  elif os.path.isdir(path):
+    site_dir = path
+  else:
+    raise ValueError('virtualenv: cannot access %s: '
+                     'No such virtualenv or site directory' % path)
+
+  sys_path = sys.path[:]
+  del sys.path[index:]
+  site.addsitedir(site_dir)
+  sys.path.extend(sys_path[index:])
+
+
+add('lib')
+
+from apiclient.discovery import build as discovery_build
+
+import unittest
+from GCSIterator import GCSIterator
+
+DEVELOPER_KEY = 'AIzaSyAtxMdn2Da20CQIRzWueYEejehJFyBXl2s'
+
+from oauth2client.client import SignedJwtAssertionCredentials
+
+client_email = '24182559640-m9p986sd0khfe2b6hc0m018f9b3bob6f@developer.gserviceaccount.com'
+with open("cheerspoint-mail-186705c1c0b3.pem") as f:
+  private_key = f.read()
+
+
+def get_authenticated_service():
+  print 'Authenticating...'
+  credentials = SignedJwtAssertionCredentials(client_email,
+                                              private_key,
+                                              'https://www.googleapis.com/auth/devstorage.full_control')
+
+  print 'Constructing Google Cloud Storage service...'
+  http = credentials.authorize(httplib2.Http())
+  return discovery_build('storage', 'v1', http=http)
+
+
+class GCSIteratorTest(unittest.TestCase):
+  def setUp(self):
+    gcs_service = get_authenticated_service()
+    bucket_name = u'cheerspoint-recipient'
+    txt_object_name = u'ipwarmup/cage@mitac.com.tw/kaichu1016_0000110.csv'
+
+    request = gcs_service.objects().get_media(bucket=bucket_name, object=txt_object_name.encode('utf8'))
+    self.test_iterator = GCSIterator(request, 2 * 1024)
+
+
+  def test_iterator(self):
+    reader = csv.DictReader(self.test_iterator, skipinitialspace=True, delimiter=',')
+    for row in reader:
+      print row
+      if row:
+        self.assertEqual({'email': 'kaichu1016+0000000@gmail.com', 'name': 'cage0000000'}, row)
+        break
+
+
+if __name__ == '__main__':
+  unittest.main()
