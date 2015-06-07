0f05606e4a478d45795905a115a073e8328674d8
cage.chung <cage.chung@gmail.com>
2015-06-07 21:34:38 +0800

    refactor parse csv. bytes read, sepcific time re-queue to continue parse job.

--- tasks/parser_handler.py
+++ tasks/parser_handler.py
@@ -9,17 +9,9 @@ from csv_parser import Parser
 
 class ParseCSVHandler(webapp2.RequestHandler):
   def post(self):
-    # parameters = pickle.loads(self.request.get('parameters'))
-    parameters = {'category': 'mitac-warmup1-day', 'recipient_skip': 0, 'sender_name': u'kaichu',
-                  'daily_capacity': 50000, 'start_time': '2015/05/26 18:00', 'ip_counts': 1,
-                  'subject': u'my test 3', 'hour_rate': 1, 'bucket_name': u'cheerspoint-recipient',
-                  'sender_email': u'kaichu1016@gmail.com',
-                  'edm_object_name': u'ipwarmup/cage@mitac.com.tw/treemallmail.html',
-                  'sendgrid_account': u'kaichu',
-                  'txt_object_name': u'ipwarmup/cage@mitac.com.tw/kaichu1016_0002100.csv',
-                  'type': u'ipwarmup', 'schedule_duration': 1}
+    parameters = pickle.loads(self.request.get('parameters'))
 
     logging.info(parameters)
 
     parser = Parser(parameters)
-    parser.run()
+    parser.run(MAX_TASKSQUEUE_EXECUTED_TIME=400)
