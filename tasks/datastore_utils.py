e73b774eb91ac996a3c702631820ff67d6a2c490
cage.chung <cage.chung@gmail.com>
2015-06-23 09:16:23 +0800

    add Mapper class

--- tasks/datastore_utils.py
+++ tasks/datastore_utils.py
@@ -1,4 +1,8 @@
 from google.appengine.ext import ndb
+from google.appengine.ext.db import Timeout
+from google.appengine import runtime
+
+import tasks
 
 ### Private stuff.
 def _process_chunk_of_items(
@@ -44,3 +48,83 @@ def page_queries(queries, fetch_page_size=20, keys_only=True):
       futures[i] = queries[i].fetch_page_async(
         fetch_page_size, start_cursor=cursor, keys_only=keys_only)
     yield results
+
+
+from google.appengine.ext import deferred
+from google.appengine.runtime import DeadlineExceededError
+
+
+class Mapper(object):
+  # Subclasses should replace this with a model class (eg, model.Person).
+  KIND = None
+
+  # Subclasses can replace this with a list of (property, value) tuples to filter by.
+  FILTERS = []
+
+  def __init__(self):
+    self.to_put = []
+    self.to_delete = []
+    self.tasks_queue = []
+
+  def map(self, entity):
+    """Updates a single entity.
+
+    Implementers should return a tuple containing two iterables (to_update, to_delete).
+    """
+    return ([], [])
+
+  def finish(self):
+    """Called when the mapper has finished, to allow for any final work to be done."""
+    pass
+
+  def get_query(self):
+    """Returns a query over the specified kind, with any appropriate filters applied."""
+    q = self.KIND.query()
+    for prop, value in self.FILTERS:
+      q = q.filter(prop == value)
+    q = q.order(self.KIND._key)
+    return q
+
+  def run(self, batch_size=100):
+    """Starts the mapper running."""
+    self._continue(None, batch_size)
+
+  def _batch_write(self):
+    """Writes updates and deletes entities in a batch."""
+    if self.to_put:
+      ndb.put_multi(self.to_put)
+      self.to_put = []
+    if self.to_delete:
+      ndb.delete_multi(self.to_delete)
+      self.to_delete = []
+
+  def _continue(self, start_key, batch_size):
+    q = self.get_query()
+    # If we're resuming, pick up where we left off last time.
+    if start_key:
+      key_prop = getattr(self.KIND, '_key')
+      q.filter(key_prop > start_key)
+    # Keep updating records until we run out of time.
+    try:
+      # Steps over the results, returning each entity and its index.
+      for i, entity in enumerate(q):
+        map_updates, map_deletes = self.map(entity)
+        self.to_put.extend(map_updates)
+        self.to_delete.extend(map_deletes)
+        # Do updates and deletes in batches.
+        if (i + 1) % batch_size == 0:
+          self._batch_write()
+        # Record the last entity we processed.
+        start_key = entity.key
+      self._batch_write()
+    except (Timeout,
+            runtime.DeadlineExceededError,
+            runtime.apiproxy_errors.CancelledError,
+            runtime.apiproxy_errors.DeadlineExceededError,
+            runtime.apiproxy_errors.OverQuotaError) as e:
+      # Write any unfinished updates to the datastore.
+      self._batch_write()
+      # Queue a new task to pick up where we left off.
+      tasks.addTask(self.tasks_queue, self._continue, start_key, batch_size)
+      return
+    self.finish()
